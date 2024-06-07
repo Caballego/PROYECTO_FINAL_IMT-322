ba0cf569d63e4d04806ec2536522326bc2555248
Caballego <victor.caballero@ucb.edu.com>
2024-06-07 19:23:39 -0400

    Actualización código

--- CODIGO/CODIGO.ino
+++ CODIGO/CODIGO.ino
@@ -1,12 +1,32 @@
 #include <Wire.h>
+//#include <PIDController.h>
 #include <LiquidCrystal_I2C.h>      
 LiquidCrystal_I2C lcd(0x27, 20, 4); 
 
 #include <SPI.h>
 
-#define MAX6675_SO 2
-#define MAX6675_CS 3
-#define MAX6675_SCK 4
+//PIDController pid;
+
+#define MAX6675_SO 5
+#define MAX6675_CS 6
+#define MAX6675_SCK 7
+#define LED_PILOTO 8
+#define RELE_PLANCHA 9
+#define BOTON_INICIO 2
+#define BOTON_STOP 3
+const int tiempo_limite = 40;
+long tiempo = 0;
+bool estado_espera = true;
+typedef enum{
+  ESPERA,
+  CALENTANDO,
+  ENFRIANDO,
+  STOP
+}estados;
+estados soldadora = ESPERA;
+//const int outputPin   = 10;
+//bool funcionamiento = true;
+//int setpoint=260;
 
 void setup()
 {
@@ -16,16 +36,94 @@ void setup()
   lcd.print("Bienvenido");
   delay(1500);
   lcd.clear();
+  pinMode(LED_PILOTO, OUTPUT);
+  digitalWrite(LED_PILOTO, LOW);
+  pinMode(RELE_PLANCHA, OUTPUT);
+  digitalWrite(RELE_PLANCHA, LOW);
+  pinMode(BOTON_INICIO, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BOTON_INICIO), iniciar_calentado, FALLING);
+  pinMode(BOTON_STOP, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BOTON_STOP), parada_emergencia, FALLING);
+  Serial.begin(9600);
+  /*pinMode(outputPin, OUTPUT);
+  pid.begin();
+  pid.setpoint(setpoint);
+  pid.tune(1, 1, 1);
+  pid.limit(0, 255);*/
 }
 
 void loop()
 {
-  float temperatura = leer_termopar();
+  Serial.println(soldadora);
+  switch (soldadora)
+  {
+    case ESPERA:
+    Serial.println("espera");
+    lcd.setCursor(4,1);
+    lcd.print("PULSE EL BOTON");
+    lcd.setCursor(5,2);
+    lcd.print("PARA INICIAR");
+    Serial.println("espera");
+    delay(300);
+    break;
+    case CALENTANDO:
+    Serial.println(soldadora);
+    float temperatura = leer_termopar();
+    lcd.setCursor(5, 1);
+    lcd.print("CALENTANDO");
+    lcd.setCursor(7, 2);
+    lcd.print(temperatura, 2);
+    if (temperatura>29)
+    {
+      soldadora=ENFRIANDO;
+    }
+    delay(300);
+    break;
+    case ENFRIANDO:
+    Serial.println(soldadora);
+    digitalWrite(RELE_PLANCHA, LOW);
+    temperatura = leer_termopar();
+    lcd.setCursor(5, 1);
+    lcd.print("ENFRIANDO");
+    lcd.setCursor(7, 2);
+    lcd.print(temperatura, 2);
+    if (temperatura<20)
+    {
+      digitalWrite(LED_PILOTO, HIGH);
+      lcd.setCursor(2, 1);
+      lcd.print("RETIRAR LA PLACA");
+      lcd.setCursor(7, 2);
+      lcd.print(temperatura, 2);
+    }
+    delay(300);
+    break;
+    case STOP:
+    Serial.println(soldadora);
+    Serial.println("kjd");
+    lcd.setCursor(1, 1);
+    lcd.print("PROGRAMA  DETENIDO");
+    lcd.setCursor(0, 2);
+    lcd.print("REINICIE EL PROGRAMA");
+    delay(300);
+    break;
+  }
+  /*float temperatura = leer_termopar();
   lcd.setCursor(2, 1);
   lcd.print("Termopar tipo K");
   lcd.setCursor(7, 2);
   lcd.print(temperatura, 2);
-  delay(300);
+  delay(300);*/
+  /*int output = pid.compute(temperatura);
+  analogWrite(outputPin, output);
+  if (funcionamiento)
+  {
+    if (temperatura=setpoint)
+    {
+      delay(10000);
+      setpoint=0;
+      funcionamiento = false;
+    }
+  }*/
 }
 
 double leer_termopar()
@@ -61,4 +159,28 @@ double leer_termopar()
 
   // The remaining bits are the number of 0.25 degree (C) counts
   return v * 0.25;
+}
+
+void iniciar_calentado()
+{
+  if (estado_espera)
+  {
+    if (millis()-tiempo > tiempo_limite)
+    {
+      soldadora = CALENTANDO;
+      digitalWrite(RELE_PLANCHA, HIGH);
+      //lcd.clear();
+      estado_espera=false;
+    }  
+  }
+}
+
+void parada_emergencia()
+{
+  if (millis()-tiempo > tiempo_limite)
+  {
+    //lcd.clear();
+    soldadora = STOP;
+    digitalWrite(RELE_PLANCHA, LOW);
+  }
 }
